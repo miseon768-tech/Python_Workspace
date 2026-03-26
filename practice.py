a30ce596e65f9c21761fcbd08368aa92228c001e
miseon768-tech <miseon768@gmail.com>
2026-03-26 19:24:07 +0900

    practice.py에 숫자, 문자열, boolean 자료형 출력 추가

--- practice.py
+++ practice.py
@@ -1,3 +1,5 @@
+
+# 숫자 자료형
 print(5)
 print(-10)
 print(3.14)
@@ -5,3 +7,20 @@ print(1000)
 print(5 + 3)
 print(2 * 8)
 print(3 * (3 + 1))
+
+
+# 문자열 자료형
+print('풍선')
+print("나비")
+print("ㅋㅋㅋㅋㅋ")
+print("ㅋ" * 9)  
+
+
+#  boolean 자료형
+print(5>10)
+print(5<10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5>10))
